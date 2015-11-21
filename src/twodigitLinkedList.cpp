c1cbf0c85a504eb05ad1cc346287a83068f83666
meshubh <shubhamagarwal625@gmail.com>
2015-11-21 11:45:37 +0530

    Submit Commit

--- src/twodigitLinkedList.cpp
+++ src/twodigitLinkedList.cpp
@@ -14,13 +14,40 @@ NOTES: Only Postive Numbers
 
 #include <stdio.h>
 #include <malloc.h>
-
+int pow_10(int n);
 struct node {
 	int digit1;
 	int digit2;
 	struct node *next;
 };
 
-int convert_sll_2digit_to_int(struct node *head){
-	return 0;
+int convert_sll_2digit_to_int(struct node *head)
+{
+	int num = 0,count=0;
+	struct node* temp;
+	temp = head;
+	while (temp)
+	{
+		count++;
+		temp = temp->next;
+	}
+	count = count * 2 - 1;
+	while (head)
+	{
+		num = num + ((head->digit1) * pow_10(count));
+		count--;
+		num = num + ((head->digit2) * pow_10(count));
+		count--;
+		head = head->next;
+	}
+	return num;
+}
+int pow_10(int n)
+{
+	int iterator, power = 1;
+	for (iterator = 0; iterator<n; iterator++)
+	{
+		power = power * 10;
+	}
+	return power;
 }
